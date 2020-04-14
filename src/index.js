6a7ed0d259f122895dcce9ba4d58f394e3225376
George Skripak <s1991gv@gmail.com>
2020-04-15 01:02:07 +0300

    - add outer styles - change structure of app - add some custom css

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
+import AppHeader from './components/app-header/app-header';
+import SearchPanel from './components/search-panel/search-panel';
+import TodoList from './components/todo-list/todo-list';
+import ItemStatusFilter from './components/item-status-filter/item-status-filter';
+import './index.css';
 
 const App = () => {
 
-    const value = '<script> alert("hui") </script>';
+    const todoData = [
+        {label: 'Drink Coffee', important: false, id: 1},
+        {label: 'Make Awesome App', important: true, id: 2},
+        {label: 'Have a lunch', important: false, id: 3}
+    ];
 
     return (
-        <div>
-            {value}
-            <AppHeader/>
-            <SearchPanel/>
-            <TodoList />
+        <div className='todo-app'>
+            <AppHeader toDo={1} done={3}/>
+            <div className='top-panel d-flex'>
+                <SearchPanel/>
+                <ItemStatusFilter/>
+            </div>
+
+            <TodoList todos={todoData}/>
         </div>
     )
 };
