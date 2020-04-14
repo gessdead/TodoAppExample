c4f0fa48576055fcfdc863017d04b332b853cc04
George Skripak <s1991gv@gmail.com>
2020-04-14 19:40:51 +0300

    - split application to components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-    <div>
-        <h1>My Todo List</h1>
-        <input type="text" placeholder="search"/>
-        <ul>
-            <li>Learn REact</li>
-            <li>Build Awesome App</li>
-        </ul>
-    </div>
-);
-console.log(el);
+const TodoList = () => {
 
-ReactDOM.render(el, document.querySelector('#root'));
+    const items = ['Drink Coffee', 'Build Awesome App'];
+
+    return (
+        <ul>
+            <li>{items[0]}</li>
+            <li>{items[1]}</li>
+        </ul>
+    )
+};
+
+const AppHeader = () => <h1>My Todo List</h1>;
+
+const SearchPanel = () => {
+
+    const searchText = 'Type here to search';
+    const searchStyle = {
+        fontSize: '20px'
+    };
+
+    return (
+        <input
+            type="text"
+            placeholder={searchText}
+            className="test"
+            htmlFor="rrr"
+            style={searchStyle}
+        />
+    )
+};
+
+const App = () => {
+
+    const value = '<script> alert("hui") </script>';
+
+    return (
+        <div>
+            {value}
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList />
+        </div>
+    )
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
